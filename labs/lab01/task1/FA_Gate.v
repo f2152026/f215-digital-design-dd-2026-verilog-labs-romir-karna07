4b6d347c388913347bfd0c145d71fc8917423c02
Romir-Karna07 <f20250612@pilani.bits-pilani.ac.in>
2026-08-29 16:59:13 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -16,11 +16,11 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor #(2) (ps,  a,   b);
-  and # (2) (pc2, cin, ps);
-  and #(5)(pc1, a,   b);
-  xor #(5)(sum, cin, ps);
-  or  #(2)(cout, pc1, pc2);
+  xor  (ps,  a,   b);
+  and  (pc2, cin, ps);
+  and (pc1, a,   b);
+  xor (sum, cin, ps);
+  or  (cout, pc1, pc2);
 
 
 
